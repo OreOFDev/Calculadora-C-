8b483f3edb7b5c6a40ebda73f9e020a086a14e7f
OreOFDev <oreofday@outlook.com>
2025-05-16 15:04:55 -0300

    Add files via upload

---
+++ OreOFDAY/OreOFDAY/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Bem Vindo A Calculadora!");
+
+Console.WriteLine("Escolha Uma Operação +, -, *, /");
+var operacao = Console.ReadLine();
+
+(double, double) LerNumeros()
+{
+    Console.WriteLine("Digite o primeiro número:");
+    var num1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Digite o segundo número:");
+    var num2 = Convert.ToDouble(Console.ReadLine());
+    return (num1, num2);
+}
+
+
+switch (operacao)
+{
+    case "+":
+        var (num1,num2) = LerNumeros();
+        Console.WriteLine($"Resultado: {num1 + num2}");
+        break;
+
+    case "-":
+        var (num3, num4) = LerNumeros();
+        Console.WriteLine($"Resultado: {num3 - num4}");
+        break;
+
+    case "*":
+        var (num5, num6) = LerNumeros();
+        Console.WriteLine($"Resultado: {num5 * num6}");
+        break;
+
+    case "/":
+        var (num7, num8) = LerNumeros();
+        Console.WriteLine($"Resultado: {num7 / num8}");
+        break;
+
+}
